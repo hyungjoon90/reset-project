d19f05cbd08f98795099c19e95489ee305147ae3
빈그림 <36255115+emptyDrawing@users.noreply.github.com>
2018-07-04 11:15:34 +0900

    [오류수정] CommonDao 오류수정

--- src/main/java/ga/beauty/reset/dao/CommonDao.java
+++ src/main/java/ga/beauty/reset/dao/CommonDao.java
@@ -4,13 +4,13 @@ import java.sql.SQLException;
 import java.util.List;
 
 
-import ga.beauty.reset.dao.entity.CommonDao;
+import ga.beauty.reset.dao.CommonDao;
 
 public interface CommonDao {
 
 	<C> List<C> selectAll() throws SQLException;
-	<C> insertOne(C bean) throws SQLException;
-	<C> void selectOne(C bean) throws SQLException;
+	<C> void insertOne(C bean) throws SQLException;
+	<C> C selectOne(C bean) throws SQLException;
 	<C> int updateOne(C bean) throws SQLException;
 	<C> int deleteOne(C bean) throws SQLException;
 	
