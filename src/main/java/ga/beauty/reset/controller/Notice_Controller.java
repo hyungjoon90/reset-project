4f8c91e26a90af713113c9e40e86477b6609cd25
JihyunLee99 <jhl7785@gmail.com>
2018-07-16 18:07:24 +0900

    [공지사항] 공지사항 수정 / 2018-07-16 / 이지현

--- src/main/java/ga/beauty/reset/controller/Notice_Controller.java
+++ src/main/java/ga/beauty/reset/controller/Notice_Controller.java
@@ -1,9 +1,6 @@
 package ga.beauty.reset.controller;
 
-import java.io.UnsupportedEncodingException;
 import java.sql.SQLException;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,8 +21,7 @@ public class Notice_Controller {
 	@Autowired
 	Notice_Service service;
 	
-	String view="redirect:/admin/notice";
-	
+	String view = "redirect:/admin/notice";
 	public Notice_Controller() {
 		// TODO Auto-generated constructor stub
 	}
@@ -36,44 +32,26 @@ public class Notice_Controller {
 		System.out.println("리스트 보여주기");
 		return "admin/notice";
 	}	
-
 	@RequestMapping(value="/admin/notice",method=RequestMethod.POST)
-	public String add(Notice_Vo bean) throws SQLException {
+	public String add(@ModelAttribute Notice_Vo bean) throws SQLException {
 		System.out.println("입력페이지, 입력전");
 		service.addPage(bean);
 		System.out.println("입력후");
 		return view;
 	}
-	
+	@RequestMapping(value="/admin/notice/{no_no}", method=RequestMethod.PUT)
+	public String edit(@PathVariable("no_no") int no_no, @ModelAttribute Notice_Vo bean, Model model) throws SQLException {
+		service.updatePage(bean);
+		model.addAttribute("no_no",no_no);
+		return view;
+	}
 	@RequestMapping(value="/admin/notice/{no_no}",method=RequestMethod.DELETE)
-	public String delete(@PathVariable int no_no) throws SQLException {
+	public String delete(@PathVariable("no_no") int no_no) throws SQLException {
 		System.out.println("삭제시도");
 		service.deletePage(no_no);
-		System.out.println("삭제완료");
+		System.out.println("삭제완료");                                                                                                                                                                                                                                                                                                                                                                                           
 		return view;
 	}
-	
-//	@RequestMapping(value="/admin/notice/{no_no}", method=RequestMethod.PUT,  produces = "text/html;charset=UTF-8")
-//	public String edit(@PathVariable int no_no
-//			, @ModelAttribute Notice_Vo bean,Model model, HttpServletRequest req) throws SQLException, UnsupportedEncodingException {
-//		req.setCharacterEncoding("utf-8");
-//		System.out.println(bean);
-//		service.updatePage(bean);
-//		model.addAttribute("no_no", no_no);
-//		return view;
-//	}
-	
-	
-	@RequestMapping(value="/admin/notice/{no_no}", method=RequestMethod.PUT)
-	public String edit(@PathVariable int no_no
-			, @ModelAttribute Notice_Vo bean,Model model, HttpServletRequest req) throws SQLException {
-		
-		System.out.println(bean);
-		service.updatePage(bean);
-		model.addAttribute("no_no", no_no);
-		return view;
-	}
-	
 
 	
 }//class end
