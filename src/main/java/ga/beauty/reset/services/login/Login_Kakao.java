eb6571adba4c72c37e6ac24d9e90e2a5c2f52734
빈그림 <36255115+emptyDrawing@users.noreply.github.com>
2018-08-03 18:18:34 +0900

    [카카오톡] mater 용으로 변경 / 2018-08-03 / 김수성

--- src/main/java/ga/beauty/reset/services/login/Login_Kakao.java
+++ src/main/java/ga/beauty/reset/services/login/Login_Kakao.java
@@ -38,7 +38,7 @@ public class Login_Kakao implements Login_Service{
 
 	private static final Logger logger = Logger.getLogger(Login_Kakao.class);
 	
-	private final String clientId = "1b9f25b372bcd6828070cc433502aa22";//애플리케이션 클라이언트 아이디값";
+	private final String clientId = "f709273524fdad8902b81660b68a0735";//애플리케이션 클라이언트 아이디값";
 	private String redirectURI ="http://reset-beauty.ga/login/kakao/"; // TODO [kss] 주소바꿀거
 	// private String access_token = "";
 	// private String refresh_token = "";
